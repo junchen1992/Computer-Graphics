70e4df78df1ae37d314768574caa4cf0fa996c82
Jun Chen <tlnd.sky@gmail.com>
2015-08-24 12:40:06 +1000

    setup lab4.

---
+++ src/lab4/ScreenSaverOGL.java
@@ -0,0 +1,97 @@
+package lab4;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class ScreenSaverOGL implements GLEventListener {
+
+	/**
+	 * ScreenSaverOGL - this is a simple screen saver that uses JOGL2 Eric
+	 * McCreath 2009, 2011, 2015
+	 * 
+	 * You need to include the jogl jar files (gluegen-rt.jar and jogl.jar). In
+	 * eclipse use "add external jars" in Project->Properties->Libaries
+	 * otherwise make certain they are in the class path. In the current linux
+	 * computers there files are in the /usr/share/java directory.
+	 * 
+	 * If you are executing from the command line then something like: javac -cp
+	 * .:/usr/share/java/jogl2.jar:/usr/share/java/gluegen2-2.2.4-rt.jar
+	 * ScreenSaverOGL.java java -cp
+	 * .:/usr/share/java/jogl2.jar:/usr/share/java/gluegen2-2.2.4-rt.jar
+	 * ScreenSaverOGL should work.
+	 *
+	 * On our lab machine you may also need to check you are using Java 7. You
+	 * can run it directly using: /usr/lib/jvm/java-7-openjdk-amd64/bin/javac
+	 * and /usr/lib/jvm/java-7-openjdk-amd64/bin/java
+	 * 
+	 */
+
+	JFrame jf;
+	GLCanvas canvas;
+	GLProfile profile;
+	GLCapabilities caps;
+	Dimension dim = new Dimension(800, 600);
+	FPSAnimator animator;
+
+	float xpos;
+	float xvel;
+
+	public ScreenSaverOGL() {
+		jf = new JFrame();
+		profile = GLProfile.getDefault();
+		caps = new GLCapabilities(profile);
+		canvas = new GLCanvas(caps);
+		canvas.addGLEventListener(this);
+		canvas.requestFocusInWindow();
+		jf.getContentPane().add(canvas);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+		jf.setPreferredSize(dim);
+		jf.pack();
+		animator = new FPSAnimator(canvas, 20);
+		xpos = 100.0f;
+		xvel = 1.0f;
+		animator.start();
+	}
+
+	public static void main(String[] args) {
+		new ScreenSaverOGL();
+	}
+
+	public void init(GLAutoDrawable dr) { // set up openGL for 2D drawing
+		GL2 gl2 = dr.getGL().getGL2();
+		GLU glu = new GLU();
+		GLUT glut = new GLUT();
+		gl2.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		gl2.glMatrixMode(GL2.GL_PROJECTION);
+		gl2.glLoadIdentity();
+		glu.gluOrtho2D(0.0, dim.getWidth(), 0.0, dim.getHeight());
+		gl2.glMatrixMode(GL2.GL_MODELVIEW);
+		gl2.glLoadIdentity();
+	}
+
+	public void display(GLAutoDrawable dr) { // clear the screen and draw "Save
+												// the Screens"
+		GL2 gl2 = dr.getGL().getGL2();
+		GLU glu = new GLU();
+		GLUT glut = new GLUT();
+
+		gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
+
+		gl2.glColor3f(1.0f, 0.0f, 0.0f);
+		gl2.glRasterPos2f(xpos, 300.0f);
+		glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18, "Save the Screens");
+		gl2.glFlush();
+
+		xpos += xvel;
+		if (xpos > dim.getWidth())
+			xpos = 0.0f;
+	}
+
+	public void dispose(GLAutoDrawable glautodrawable) {
+	}
+
+	public void reshape(GLAutoDrawable dr, int x, int y, int width, int height) {
+	}
+}
